ed7d3f9f271d0757a6fd7f137a7af66232ae3b1d
frazao090 <141861827+frazao090@users.noreply.github.com>
2023-10-26 12:50:05 -0300

    correção após sugestão

--- script.js
+++ script.js
@@ -14,15 +14,38 @@ function playRockPaperScissor(player1, player2) {
         (player1 == 'Pedra' && player2 == 'Papel')
     ) {
         return 'Jogador 2 venceu!'
-    } else if (
-        (player1 == 'Pedra' && player2 == 'Pedra') ||
-        (player1 == 'Tesoura' && player2 == 'Tesoura') ||
-        (player1 == 'Papel' && player2 == 'Papel')
-    ) {
+    } else {
         return 'Empate!'
     }
 }
 
-const resultado = playRockPaperScissor('Pedra', 'Pedra')
-console.log(resultado)
+const resultado = playRockPaperScissor('Tesoura', 'Tesoura')
 
+
+
+
+
+
+// if (
+//     (player1 == 'Pedra' && player2 == 'Tesoura') ||
+//     (player1 == 'Tesoura' && player2 == 'Papel') ||
+//     (player1 == 'Papel' && player2 == 'Pedra')
+// ) {
+//     return 'Jogador 1 venceu!'
+// } else if (
+//     (player1 == 'Tesoura' && player2 == 'Pedra') ||
+//     (player1 == 'Papel' && player2 == 'Tesoura') ||
+//     (player1 == 'Pedra' && player2 == 'Papel')
+// ) {
+//     return 'Jogador 2 venceu!'
+// } else if (
+//     (player1 == 'Pedra' && player2 == 'Pedra') ||
+//     (player1 == 'Tesoura' && player2 == 'Tesoura') ||
+//     (player1 == 'Papel' && player2 == 'Papel')
+// ) {
+//     return 'Empate!'
+// }
+// }
+
+// const resultado = playRockPaperScissor('Pedra', 'Pedra')
+// console.log(resultado)
